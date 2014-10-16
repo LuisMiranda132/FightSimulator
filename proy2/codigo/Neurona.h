cb81d647408b9f2b1db75a6c3d2f605eb7ba2b8d
jmontene <josmontenegro2012@gmail.com>
2014-10-16 16:55:17 -0430

    Resueltos los errores. Falta probar. El caso que tenia arroja resultados dudosos

--- proy2/codigo/Neurona.h
+++ proy2/codigo/Neurona.h
@@ -6,28 +6,28 @@
 using namespace std;
 
 //Typedef para los ejemplos
-typedef pair< vector<float> ,float > ejemplo;
+typedef pair< vector<double> ,double > ejemplo;
 
 class Neurona{
    public:
-      vector<float> pesos;
-      Neurona(vector<float> w): pesos(w) {}
-      virtual void aprender(vector<ejemplo>,float) = 0;
-      virtual float resolver(vector<float>) = 0;
+      vector<double> pesos;
+      Neurona(vector<double> w): pesos(w) {}
+      virtual void aprender(vector<ejemplo>,double) = 0;
+      virtual double resolver(vector<double>) = 0;
 };
 
 
 //****************CLASE PERCEPTRON**************
 class Perceptron : public Neurona {
    public:
-      vector<float> pesos;
-      Perceptron(vector<float> w): Neurona(w) {}
-      void aprender(vector<ejemplo>, float);
-      float resolver(vector<float>);
+      vector<double> pesos;
+      Perceptron(vector<double> w): Neurona(w) {}
+      void aprender(vector<ejemplo>, double);
+      double resolver(vector<double>);
 };
 
-float Perceptron::resolver(vector<float> input){
-   float output = 0;
+double Perceptron::resolver(vector<double> input){
+   double output = 0;
    for(int i=0;i<input.size();++i){
       output += pesos[i] * input[i];
    }
@@ -35,26 +35,26 @@ float Perceptron::resolver(vector<float> input){
    else return -1;
 }
 
-void Perceptron::aprender(vector<ejemplo> ejemplos, float tasa){
+void Perceptron::aprender(vector<ejemplo> ejemplos, double tasa){
    random_device rd;
    default_random_engine generator(rd());
-   uniform_real_distribution<float> distribution(0.0f,1.0f);
+   uniform_real_distribution<double> distribution(0.0f,1.0f);
 
-   vector<float> delta(pesos);
+   vector<double> delta(pesos);
 
    for(int i=0;i<pesos.size();++i){
       pesos[i] = distribution(generator);
    }
 
    int solved = 0;
-   float t_e;
+   double t_e;
    int it = 0;
    while(solved != ejemplos.size()){
       solved = 0;
       t_e = 0.0f;
       for(int i=0;i<ejemplos.size();++i){
-	 float o = resolver(ejemplos[i].first);
-	 float e = ejemplos[i].second - o;
+	 double o = resolver(ejemplos[i].first);
+	 double e = ejemplos[i].second - o;
 	 if(o == ejemplos[i].second) ++solved;
 	 for(int j=0;j<pesos.size();++j){
 	    pesos[j] += tasa*e*ejemplos[i].first[j];
@@ -62,7 +62,7 @@ void Perceptron::aprender(vector<ejemplo> ejemplos, float tasa){
 	 t_e += e*e;
       }
       t_e /= 2.0f;
-      cout << it++ << "   " << t_e << endl;
+      //cout << it++ << "   " << t_e << endl;
    }
 }
 
@@ -71,36 +71,35 @@ void Perceptron::aprender(vector<ejemplo> ejemplos, float tasa){
 //****************CLASE ADALINE**************
 class Adaline : public Neurona{
    public:
-      vector<float> pesos;
-      Adaline(vector<float> w): Neurona(w) {}
-      void aprender(vector<ejemplo>, float);
-      float resolver(vector<float>);
+      Adaline(vector<double> w): Neurona(w) {}
+      void aprender(vector<ejemplo>, double);
+      double resolver(vector<double>);
 };
 
-float Adaline::resolver(vector<float> input){
-   float output = 0;
+double Adaline::resolver(vector<double> input){
+   double output = 0;
    for(int i=0;i<input.size();++i){
       output += pesos[i] * input[i];
    }
    return output; 
 }
 
-void Adaline::aprender(vector<ejemplo> ejemplos, float tasa){
+void Adaline::aprender(vector<ejemplo> ejemplos, double tasa){
    random_device rd;
    default_random_engine generator(rd());
-   uniform_real_distribution<float> distribution(0.0f,1.0f);
+   uniform_real_distribution<double> distribution(0.0f,1.0f);
 
    for(int i=0;i<pesos.size();++i){
       pesos[i] = distribution(generator);
    }
 
-   float t_e = 15.0f;
+   double t_e = 15.0f;
    int it = 1;
    while(abs(t_e) > 14.694f){
       t_e = 0.0f;
       for(int i=0;i<ejemplos.size();++i){
-	 float o = resolver(ejemplos[i].first);
-	 float e = o * (ejemplos[i].second - o);
+	 double o = resolver(ejemplos[i].first);
+	 double e = o * (ejemplos[i].second - o);
 	 for(int j=0;j<pesos.size();++j){
 	    pesos[j] += tasa*e*ejemplos[i].first[j];
 	 }
@@ -116,43 +115,41 @@ void Adaline::aprender(vector<ejemplo> ejemplos, float tasa){
 //****************CLASE SIGMOIDAL**************
 class Sigmoidal : public Neurona{
    public:
-      vector<float> pesos;
-      Sigmoidal(vector<float> w): Neurona(w) {}
-      void aprender(vector<ejemplo>, float);
-      float resolver(vector<float>);
+      Sigmoidal(vector<double> w): Neurona(w) {}
+      void aprender(vector<ejemplo>, double);
+      double resolver(vector<double>);
 };
 
-float Sigmoidal::resolver(vector<float> input){
-   float output = 0;
+double Sigmoidal::resolver(vector<double> input){
+   double output = 0;
    for(int i=0;i<input.size();++i){
-      cout << this->pesos.size() << endl;
       output += pesos[i] * input[i];
    }
-   return 1.0f/(1.0f + 1.0f/(exp(output)));
+   return 1.0/(1.0 + exp(-output));
 }
 
-void Sigmoidal::aprender(vector<ejemplo> ejemplos, float tasa){
+void Sigmoidal::aprender(vector<ejemplo> ejemplos, double tasa){
    random_device rd;
    default_random_engine generator(rd());
-   uniform_real_distribution<float> distribution(0.0f,1.0f);
+   uniform_real_distribution<double> distribution(0.0,1.0);
 
    for(int i=0;i<pesos.size();++i){
       pesos[i] = distribution(generator);
    }
 
-   float t_e = 15.0f;
+   double t_e = 15.0;
    int it = 1;
-   while(abs(t_e) > 14.694f){
-      t_e = 0.0f;
+   while(abs(t_e) > 14.694){
+      t_e = 0.0;
       for(int i=0;i<ejemplos.size();++i){
-	 float o = resolver(ejemplos[i].first);
-	 float e = (ejemplos[i].second - o);
+	 double o = resolver(ejemplos[i].first);
+	 double e = (ejemplos[i].second - o);
 	 for(int j=0;j<pesos.size();++j){
 	    pesos[j] += tasa*e*ejemplos[i].first[j];
 	 }
 	 t_e += e*e;
       }
-      t_e /= 2.0f;
+      t_e /= 2.0;
       cout << it++ << "   " << t_e << endl;
    }
 }
--- proy2/codigo/RedNeural.h
+++ proy2/codigo/RedNeural.h
@@ -2,28 +2,28 @@
 using namespace std;
 
 typedef vector<Neurona*> capa;
-typedef vector< vector<float> > salida;
-typedef pair<vector<float>, vector<float> > ejemplo2;
+typedef vector< vector<double> > salida;
+typedef pair<vector<double>, vector<double> > ejemplo2;
 
 class RedNeural{
    public:
       vector<capa> capas;
       RedNeural(vector<capa> c): capas(c) {};
-      void aprender(vector<ejemplo2>, float);
-      salida resolver(vector<float>);
-      float getSum(capa, vector<float>);
+      void aprender(vector<ejemplo2>, double);
+      salida resolver(vector<double>);
+      double getSum(capa, vector<double>);
 };
 
-salida RedNeural::resolver(vector<float> input){
+salida RedNeural::resolver(vector<double> input){
    salida output;
-   vector<float> t1;
+   vector<double> t1;
    for(int i=0;i<capas[0].size();++i){
       t1.push_back(capas[0][i]->resolver(input));
    }
    output.push_back(t1);
 
    for(int i=1;i<capas.size();++i){
-      vector<float> temp;
+      vector<double> temp;
       for(int j=0;j<capas[i].size();++j){
 	 temp.push_back(capas[i][j]->resolver(output[i-1]));
       }
@@ -33,19 +33,19 @@ salida RedNeural::resolver(vector<float> input){
    return output;
 }
 
-void RedNeural::aprender(vector<ejemplo2> ejemplos, float tasa){
+void RedNeural::aprender(vector<ejemplo2> ejemplos, double tasa){
    random_device rd;
    default_random_engine gen(rd());
-   uniform_real_distribution<float> distribution(0.0f,1.0f);
+   uniform_real_distribution<double> distribution(0.0,1.0);
 
-   vector<float> treble_k;
+   vector<double> treble_k;
    salida treble_h;
 
    for(int i=0;i<capas.size();++i){
-      vector<float> t2;
+      vector<double> t2;
       for(int j=0;j<capas[i].size();++j){
-	 if(i == capas.size()-1) treble_k.push_back(0.0f);
-	 else t2.push_back(0.0f);
+	 if(i == capas.size()-1) treble_k.push_back(0.0);
+	 else t2.push_back(0.0);
 	 for(int k=0;k<capas[i][j]->pesos.size();++k){
 	    capas[i][j]->pesos[k] = distribution(gen);
 	 }
@@ -53,40 +53,54 @@ void RedNeural::aprender(vector<ejemplo2> ejemplos, float tasa){
       if(i != capas.size()-1) treble_h.push_back(t2);
    }
 
-   float t_e = 10.0f;
-   while(abs(t_e) > 0.1f){
-      t_e = 0.0f;
+   double t_e = 10.0;
+   while(abs(t_e) > 0.1){
+      t_e = 0.0;
       for(int i=0;i<ejemplos.size();++i){
 	 salida o = resolver(ejemplos[i].first);
-	 vector<float> t_k = ejemplos[i].second;
+	 vector<double> t_k = ejemplos[i].second;
 	 for(int j=0;j<capas[capas.size()-1].size();++j){
-	    float o_k = o[capas.size()-1][j];
+	    double o_k = o[capas.size()-1][j];
+	    //cout << o_k << " vs " << t_k[j] << endl;
 	    treble_k[j] = o_k*(1-o_k)*(t_k[j]-o_k);
 	    t_e += (t_k[j]-o_k)*(t_k[j]-o_k);
+	    //cout << t_e << endl;
 	 }
 	 for(int j=0;j<capas.size()-1;++j){
 	    for(int k=0;k<capas[j].size();++k){
-	       float o_h = o[j][k];
+	       double o_h = o[j][k];
 	       treble_h[j][k] =
 		  o_h*(1-o_h)*getSum(capas[capas.size()-1],treble_k);
 	    }
 	 }
-	 for(int j=0;i<capas.size();++i){
-	    for(int k=0;j<capas[j].size();++j){
-	       if(j == 0) capas[j][k]->pesos[k] =
-		  tasa*treble_h[j][k]*ejemplos[i].first[k]; 
-	       else capas[j][k]->pesos[k] = 
-		  tasa*treble_h[j][k]*o[j-1][k];
+	 for(int j=0;j<capas.size();++j){
+	    for(int k=0;k<capas[j].size();++k){
+	       if(j == 0){
+		  for(int l=0;l<capas[j][k]->pesos.size();++l){
+		     capas[j][k]->pesos[l] =
+		     tasa*treble_h[j][k]*ejemplos[i].first[l];
+		  }
+	       }else if(j != capas.size()-1){
+		  for(int l=0;l<capas[j][k]->pesos.size();++l){
+		     capas[j][k]->pesos[l] = 
+		     tasa*treble_h[j][k]*o[j-1][l];
+		  }
+	       }else{
+		  for(int l=0;l<capas[j][k]->pesos.size();++l){
+		     capas[j][k]->pesos[l] = tasa*treble_k[k]*o[j-1][l];
+		  }
+	       }
 	    }
 	 }
       }
-      t_e /= 2.0f;
+      t_e /= 2.0;
+      //cout << t_e << endl;
    }
 
 }
 
-float RedNeural::getSum(capa c, vector<float> t){
-   float res = 0.0f;
+double RedNeural::getSum(capa c, vector<double> t){
+   double res = 0.0;
 
    for(int i=0;i<c.size();++i){
       for(int j=0;j<c[i]->pesos.size();++j){
