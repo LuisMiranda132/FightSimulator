ed339f2065a5d6d47ee69e6640d9ef780e614b48
jmontene <josmontenegro2012@gmail.com>
2014-11-04 14:23:35 -0430

    Terminado gabil con argumentos. Se incluyen los datos de prueba

--- proy3/gabil.py
+++ proy3/gabil.py
@@ -2,15 +2,18 @@ from pyevolve import G1DList
 from pyevolve import GSimpleGA
 from pyevolve import G1DBinaryString
 from pyevolve import Util
+from pyevolve import Selectors
 from random import randint as rand_randint, uniform as rand_uniform, choice as rand_choice, randrange as randrange
 import sys
 
 
-MAX_SET_SIZE = 5
+MAX_SET_SIZE = 3
 RULE_SIZE = 36
 INITIAL_POP = 10
 TRAINING_SET = []
+i = 0
 
+##################################Atributos######################################
 def attr1(num):
    num = float(num)
    if num < 4:
@@ -95,14 +98,7 @@ def attr5(num):
       return '10'
    return '01'
 
-def eval_func(chromosome):
-   score = 0.0
-   # iterate over the chromosome
-   for value in chromosome:
-      if value==0:
-         score += 1
-   return score/len(chromosome.genomeList)
-
+###############################Inicializacion#####################################
 def init_func(genome, **args):
    the_set = []
    set_size = randrange(1,MAX_SET_SIZE+1)
@@ -111,10 +107,15 @@ def init_func(genome, **args):
       the_set = the_set + rule
    genome.genomeList = the_set
 
-def match(chromosome,sample):
+#################################Fitness#########################################
+def match(chromosome,sample,opt=0):
    s = long(sample,2)
-   for i in range(0,len(chromosome.genomeList)-1,RULE_SIZE):
-      if (long(''.join(chromosome.genomeList[i:i+RULE_SIZE-1]),2) & s) == s:
+   c = ''.join(chromosome.genomeList)
+   for i in range(0,len(c),RULE_SIZE):
+      if(opt == 1):
+	 print "rule  : " + c[i:i+RULE_SIZE]
+	 print "sample: " + sample
+      if (long(c[i:i+RULE_SIZE],2) & s) == s:
 	 return True
    return False
 
@@ -123,10 +124,17 @@ def standard_fitness(chromosome):
    for sample in TRAINING_SET:
       if(match(chromosome,sample)):
 	 score+=1
-   return pow(score/len(TRAINING_SET),2)
+   return pow(score,2)
 
+def size_fitness(chromosome):
+   score = 0
+   for sample in TRAINING_SET:
+      if(match(chromosome,sample)):
+	 score+=1
+   return int(float(pow(score,2)) + 1.0/(pow(len(chromosome.genomeList)/RULE_SIZE,2)))
+
+##############################Crossover##########################################
 def gabil_cross(genome, **args):
-# def gabil_cross(genome):
    sister = None
    brother = None
    gMom = args["mom"]
@@ -176,7 +184,40 @@ def gabil_cross(genome, **args):
    brother.genomeList = gDad[:newcuts[0]] + gMom[cuts[0]:cuts[1]] + gDad[newcuts[1]:]
    return (sister, brother)
 
+################################Mutacion#########################################
+def gabil_mutation(genome, **args):
+  
+   if args["pmut"] <= 0.0: return 0
+   stringLength = len(genome)
+   mutations = args["pmut"] * (stringLength)
+
+   if mutations < 1.0:
+      mutations = 0
+      for it in xrange(stringLength):
+	 if Util.randomFlipCoin(args["pmut"]):
+	    if genome.genomeList[it] == '0': genome.genomeList[it] = '1'
+	    else: genome.genomeList[it] = '0'
+	    mutations+=1
+   else:
+      for it in xrange(int(round(mutations))):
+	 which = rand_randint(0,stringLength-1)
+	 if genome.genomeList[which] == '0': genome.genomeList[which] = '1'
+	 else: genome.genomeList[which] = '0'
+
+   return int(mutations)
+
+####################################Main#########################################
 ###Definir el conjunto de entrenamiento###
+if len(sys.argv) != 7:
+   response = "usage: python gabil.py <trainingFile>"
+   response += " <sampleFile> <selector> <fitness>"
+   response += " <mutationRate> <crossoverRate>\n"
+   response += "selector must be either 1(Rank) or 2(Roulette)\n"
+   response += "fitness must be either 1(Standard) or 2(Size)"
+   print response
+   sys.exit()
+
+
 f = open(sys.argv[1],'r')
 for line in f:
    l = line.split(" ")
@@ -188,13 +229,49 @@ for line in f:
    t = t + attr5(l[4])
    TRAINING_SET = TRAINING_SET + [t]
 
-
 genome = G1DBinaryString.G1DBinaryString(MAX_SET_SIZE)
 genome.initializator.set(init_func)
-genome.evaluator.set(standard_fitness)
+
+if(int(sys.argv[4]) == 1): genome.evaluator.set(standard_fitness)
+else: genome.evaluator.set(size_fitness)
+
 genome.crossover.set(gabil_cross)
+genome.mutator.set(gabil_mutation)
 ga = GSimpleGA.GSimpleGA(genome)
-ga.setGenerations(50)
+ga.terminationCriteria.set(GSimpleGA.FitnessStatsCriteria)
+
+if(int(sys.argv[3]) == 1): ga.selector.set(Selectors.GRankSelector)
+else: ga.selector.set(Selectors.GRouletteWheel)
+ga.setMutationRate(float(sys.argv[5]))
+ga.setCrossoverRate(float(sys.argv[6]))
+ga.setGenerations(100)
 ga.setPopulationSize(INITIAL_POP)
-ga.evolve(freq_stats=10)
-print ga.bestIndividual()
+ga.evolve(freq_stats=0)
+
+f.close()
+f = open(sys.argv[2],'r')
+SAMPLE_SET = []
+for line in f:
+   l = line.split(" ")
+   t = ""
+   t = t + attr1(l[0])
+   t = t + attr2(l[1])
+   t = t + attr3(l[2])
+   t = t + attr4(l[3])
+   t = t + attr5(l[4])
+   SAMPLE_SET = SAMPLE_SET + [t]
+
+score = 0
+for sample in SAMPLE_SET:
+   if(match(ga.bestIndividual(),sample)):
+      score+=1
+
+#response = "Porcentaje de casos correctamente clasificados"
+#response += " (del 30% de los datos de la base): " 
+#response += str((float(score)/len(SAMPLE_SET)) * 100) + " por ciento"
+#print response
+
+#print ' '.join(ga.bestIndividual().genomeList)
+print float(score)/len(SAMPLE_SET)
+
+f.close()
